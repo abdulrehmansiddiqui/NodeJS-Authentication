d5cc3f81e203f627cae7f36efc8a479200151cf4
abdulrehmansiddiqui <arehmans@live.com>
2020-01-16 20:49:57 +0500

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 const { mongoUrl } = require('./key')
 
